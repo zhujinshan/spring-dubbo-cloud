20ed1213d050466108fc9b40bc61699d21eb8040
zhujinshan <1066391791@qq.com>
2019-01-21 15:08:31 +0800

    common

---
+++ common-springcloud/user-module/user-service-client/src/main/java/com/zhujs/user/client/UserClient.java
@@ -0,0 +1,19 @@
+package com.zhujs.user.client;
+
+
+import com.zhujs.user.dto.UserDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @Description: TODO
+ * @Author: jinshan.zhu
+ * @Date: 2019/1/21 14:17
+ */
+@FeignClient(path = "user-service")
+public interface UserClient {
+
+    @GetMapping(name = "获取用户", path = "user/get")
+    UserDTO get(@RequestParam("id") Integer id);
+}
--- common-springcloud/user-module/user-service-provider02/src/main/java/com/zhujs/user/UserApplication02.java
+++ common-springcloud/user-module/user-service-provider02/src/main/java/com/zhujs/user/UserApplication02.java
@@ -3,10 +3,12 @@ package com.zhujs.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EnableFeignClients
 @MapperScan(basePackages = "com.zhujs.user.mapper")
 public class UserApplication02 {
     public static void main(String[] args) {
